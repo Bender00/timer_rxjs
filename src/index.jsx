8c8e86546271526ca51e1bfe199ee32535fc83fb
Andrii Tykhonov <andriitukhonov@gmail.com>
2021-07-26 03:09:39 +0300

    version 0.0.1
    версія без css

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Proect from './scripts/proect';
+
+ReactDom.render(
+    <React.StrictMode>
+        < Proect />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
---
+++ src/scripts/proect.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import {interval} from "rxjs";
+import { map} from "rxjs/operators";
+
+import Timer from "../Timer/timer";
+
+// Крок інтервалу
+const step = 1000;
+
+function Proect() {
+
+    // дефолний стан секундоміра
+    const [timer, setTimer] = useState(0);
+    const [diff, setDiff] = useState(0);
+
+    // дефолтне значення підписки
+    const [subscription, setSubscription] = useState("");
+    const [prevent, setPrevent] = useState(true);
+
+// ****КНОПКИ****
+
+    // кнопка Start / Stop
+    const start_stopTimer = () => {
+        // Перевірка підписки
+        if (!subscription) {
+            const timerSubscription = interval(step)
+                .pipe(map((v) => v + 1))
+                .subscribe((v) => {
+                    setTimer(v + diff);
+            });
+            setSubscription(timerSubscription);
+        } else {
+            subscription.unsubscribe();
+            setTimer(0);
+            setDiff(0);
+            setSubscription("");
+        }
+    };
+    // кнопка Wait
+    const waitTimer = (event) => {
+        if (prevent) {
+            setPrevent(false);
+            const timerInstance = setTimeout(function () {
+                setPrevent(true);
+                clearTimeout(timerInstance);
+                            
+            }, 200);
+        } else {
+            if (subscription) {
+                subscription.unsubsribe();
+
+            }
+            setDiff(timer);
+            setSubscription("");
+        }
+    };
+
+    // кнопка reset 
+    const resetTimer = () => {
+        if (subscription) {
+            subscription.unsubsribe()
+
+        }
+    const timerSubscription = interval(step).subscribe((v) => {
+        setTimer(v);
+
+    });
+    setSubscription(timerSubscription);
+
+    };
+
+    return (
+        <div className="Proect">
+            <h1>Таймер</h1>
+            <Timer timePassed={timer ? timer : diff} />
+            <div className="btn">
+                <button className="buttons" id="start_stop_btn" onClick={start_stopTimer}>Start/Stop</button>
+                <button className="buttons" onClick={waitTimer}>Wait</button>
+                <button className="buttons" id="reset_btn" onClick={resetTimer}>Reset</button>
+            </div>
+        </div>
+    )
+}
+export default Proect;
